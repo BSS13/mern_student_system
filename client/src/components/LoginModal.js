4d6759bd4a51082a79160f9914f452f92311b82d
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2019-11-21 16:09:16 +1100

    Login Modal Styled

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Form,FormGroup,Input,Label,Modal,ModalHeader,ModalBody,NavLink,Button,Row,Col} from 'reactstrap';
+import {Form,FormGroup,Input,Label,Modal,ModalHeader,ModalBody,NavLink,Button} from 'reactstrap';
 import LoginModalLogo from '../images/login.png';
 class LoginModal extends Component{
 
@@ -29,33 +29,54 @@ class LoginModal extends Component{
             <div>
                 <NavLink onClick={this.toggle} href="#">Login</NavLink>
                 
-                <Modal isOpen={this.state.modal} toggle={this.toggle}>
-                    <ModalHeader toggle={this.toggle}>Login</ModalHeader>
+                    <Modal isOpen={this.state.modal} toggle={this.toggle} style={loginModal}>
+                    <ModalHeader toggle={this.toggle} style={{backgroundColor:'#3d70b2',color:'#dfe3e6',textAlign:'right',marginBottom:'8px'}}><span style={{textAlign:'center',display:'block',margin:'auto',textSize:'32px',padding:'4px'}}>Login</span></ModalHeader>
 
                     <ModalBody>
-                        <img src={LoginModalLogo} alt="Login" width="150px" height="150px"/>
+                        <img src={LoginModalLogo} alt="Login" width="150px" height="150px" style={loginLogo}/>
+                        
+                            
+
                         <Form onSubmit={this.onSubmit}>
                              <FormGroup>
-                                 <Row>
-                                     <Col className="col-sm-3"><Label for="email">Email</Label></Col>
-                                     <Col className="col-sm-3"><Input type="email" name="email" id="email" placeholder="Email" className="mb-3" onChange={this.onChange}/></Col>
-                                </Row>
+                                <Input type="email" name="email" id="email" placeholder="Email" onChange={this.onChange} style={inputStyle}/>
                             </FormGroup>
 
 
                             <FormGroup>
-                                <Label for="password" className=".col-3">Password</Label>
-                                <Input type="password" name="password" id="password" placeholder="Password" className="mb-3" onChange={this.onChange}/>
+                                <Input type="password" name="password" id="password" placeholder="Password" onChange={this.onChange} style={inputStyle}/>
                            </FormGroup>
    
-                           <Button>Login</Button>
+                           <Button style={{marginTop:'2rem',backgroundColor:'#3d70b2',color:'#dfe3e6',width:'50%',padding:'6px',margin:'auto',display:'block',marginTop:'20px',marginBottom:'10px'}}>Login</Button>
                             
                         </Form>
+                      
                     </ModalBody>
                 </Modal>
+                   
+                
+              
             </div>
         )
     }
 }
 
+const loginModal={
+    width:'700px',
+    margin:'auto',
+}
+
+const loginLogo={
+    display:'block',
+    margin:'auto',
+    marginBottom:'10px'
+}
+
+const inputStyle={
+    width:'90%',
+    display:'block',
+    margin:'auto',
+    marginBottom:'4px',
+    padding:'8px'
+}
 export default LoginModal;
