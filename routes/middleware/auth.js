806bd7fcbe82908a631972f6ea115e3ebbfa52a5
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2019-11-26 22:13:53 +1100

    JWT setup completed by creating the middleware and secret

---
+++ routes/middleware/auth.js
@@ -0,0 +1,25 @@
+const config=require('config');
+const jwt=require('jsonwebtoken');
+
+function auth(req,res,next){
+     //Firstly fetch the token from the header
+    const token=req.header('x-auth-token');
+
+    if(!token){
+        return res.status(401).json({msg:'No Token Found, Authorization Denied'});
+    }
+
+    //Have the JWT code within the try and catch block
+    try{
+        const decoded=jwt.verify(token,config.get('jwtSecret'));
+ 
+        req.user=decoded;
+        next();
+    }
+
+    catch(e){
+        res.status(400).json({msg:'Token is not Valid'});
+    }
+
+
+}
