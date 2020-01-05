48393b540301659f5561d53a7da2281a9f33e30d
Balpreet Singh Sandhu <singhbalpreet.s.sandhu@gmail.com>
2020-01-05 14:59:02 +1100

    Users controller to ensure logic and route seperation

---
+++ controllers/usersController.js
@@ -0,0 +1,114 @@
+import React from 'react';
+
+const getUser = (req,res)=>{
+    User.findById(req.user.id)
+    .select("-password")
+    .select("-cpassword")
+    .then(user=>res.json(user))
+};
+
+const signup =(req,res)=>{
+    const {username,email,password,cpassword}=req.body;
+
+    //Handle Empty Data
+    if(!username || !email || !password || !cpassword){
+        return res.status(400).json({msg:'Please Enter All Details'});
+    }
+
+    //Check if the password and confirm password match or not
+    if(password!==cpassword)
+    {
+        return res.status(400).json({msg:'Password and Confirm Password Do Not Match'});
+    }
+
+    //Check if username already taken or not
+    User.findOne({email:email})
+    .then(user=>{
+        if(user) return res.status(400).json({msg:'Email Address Already Regsitered'});
+
+        const newUser=new User({
+            username,
+            email,
+            password,
+            cpassword
+        });
+ 
+        //Generate a encrypted password
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(newUser.password,salt,(err,hash)=>{
+                if(err) throw err;
+                newUser.password=hash;
+
+                newUser.save()
+                .then(user=>{
+
+                    //Create a token when User successfully registers
+                    jwt.sign(
+                        {id:user.id},
+                        config.get('jwtSecret'),
+                        {expiresIn:7200},
+                        (err,token)=>{
+                            if(err) throw err;
+
+                            res.json({
+                                token:token,
+                                user:{
+                                    id:user.id,
+                                    name:user.username,
+                                    email:user.email
+                                }
+                            });
+                        }
+                    )
+                })
+            })
+        })
+        
+    })
+
+};
+
+const login = (req,res)=>{
+    const {email,password}=req.body;
+
+    //Check for empty
+    if(!email || !password){
+        return res.status(400).json({msg:'Please Enter Complete Details'});
+    }
+
+    User.findOne({email:email})
+    .then(user=>{
+        if(user==null) return res.status(400).json({msg:'No Such Record Exists'});
+
+
+        //Code modified for encrypted password check when logging in 
+
+        bcrypt.compare(password,user.password)
+        .then(isMatch=>{
+            if(!isMatch) return res.status(400).json({msg:'Sorry! Your password does not match'});
+            
+            jwt.sign(
+                {id:user.id},
+                config.get('jwtSecret'),
+                {expiresIn:7200},
+                (err,token)=>{
+                    if(err) throw err;
+
+                    res.json(({
+                       token,
+                       user:{
+                           id:user.id,
+                           name:user.username,
+                           email:user.email
+                       } 
+                    }))
+                }
+            )
+        })
+
+    })
+};
+
+exports.getUser = getUser;
+exports.signup = signup;
+exports.login = login;
--- routes/api/users.js
+++ routes/api/users.js
@@ -6,119 +6,17 @@ const bcrypt=require('bcryptjs');
 const jwt=require('jsonwebtoken');
 const config=require('config');
 const auth=require('../middleware/auth');
+const usersController = require('../../controllers/usersController');
 
 //Route to fetch the details of the user involving use of Middleware
-router.get("/user",auth,(req,res)=>{
-    User.findById(req.user.id)
-    .select("-password")
-    .select("-cpassword")
-    .then(user=>res.json(user));
-})
+router.get("/user",auth,usersController.getUser);
 
 //Route for the Registeration
-router.post("/",(req,res)=>{
-    const {username,email,password,cpassword}=req.body;
-
-    //Handle Empty Data
-    if(!username || !email || !password || !cpassword){
-        return res.status(400).json({msg:'Please Enter All Details'});
-    }
-
-    //Check if the password and confirm password match or not
-    if(password!==cpassword)
-    {
-        return res.status(400).json({msg:'Password and Confirm Password Do Not Match'});
-    }
-
-    //Check if username already taken or not
-    User.findOne({email:email})
-    .then(user=>{
-        if(user) return res.status(400).json({msg:'Email Address Already Regsitered'});
-
-        const newUser=new User({
-            username,
-            email,
-            password,
-            cpassword
-        });
- 
-        //Generate a encrypted password
-        bcrypt.genSalt(10,(err,salt)=>{
-            bcrypt.hash(newUser.password,salt,(err,hash)=>{
-                if(err) throw err;
-                newUser.password=hash;
-
-                newUser.save()
-                .then(user=>{
-
-                    //Create a token when User successfully registers
-                    jwt.sign(
-                        {id:user.id},
-                        config.get('jwtSecret'),
-                        {expiresIn:7200},
-                        (err,token)=>{
-                            if(err) throw err;
-
-                            res.json({
-                                token:token,
-                                user:{
-                                    id:user.id,
-                                    name:user.username,
-                                    email:user.email
-                                }
-                            });
-                        }
-                    )
-                })
-            })
-        })
-        
-    })
-
-});
+router.post("/",usersController.signup);
 
 
 //Route for the Login
-router.post("/login",(req,res)=>{
-    const {email,password}=req.body;
-
-    //Check for empty
-    if(!email || !password){
-        return res.status(400).json({msg:'Please Enter Complete Details'});
-    }
-
-    User.findOne({email:email})
-    .then(user=>{
-        if(user==null) return res.status(400).json({msg:'No Such Record Exists'});
-
-
-        //Code modified for encrypted password check when logging in 
-
-        bcrypt.compare(password,user.password)
-        .then(isMatch=>{
-            if(!isMatch) return res.status(400).json({msg:'Sorry! Your password does not match'});
-            
-            jwt.sign(
-                {id:user.id},
-                config.get('jwtSecret'),
-                {expiresIn:7200},
-                (err,token)=>{
-                    if(err) throw err;
-
-                    res.json(({
-                       token,
-                       user:{
-                           id:user.id,
-                           name:user.username,
-                           email:user.email
-                       } 
-                    }))
-                }
-            )
-        })
-
-    })
-})
+router.post("/login",usersController.login);
 
 
 
